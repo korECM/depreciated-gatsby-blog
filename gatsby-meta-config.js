033ab0152356579d5154e770c82da3f9a4d1d79e
JongwonYoun <jeffyoun@naver.com>
2021-04-20 23:48:46 +0900

    feat: ga 설정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,10 +3,10 @@ module.exports = {
   description: `ECM's Blog`,
   author: `ECM`,
   introduction: ``,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  siteUrl: `https://cookieblog.vercel.app`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `https://github.com/korECM`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
@@ -16,16 +16,16 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'korECM/blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '0', // Add your google analytics tranking ID
+  ga: '', // Add your google analytics tranking ID
 }
