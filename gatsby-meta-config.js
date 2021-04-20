d862f852419481ee60a219d5dacd171bcdb00a4d
JongwonYoun <jeffyoun@naver.com>
2021-04-20 23:57:55 +0900

    fix: github 링크 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -6,7 +6,7 @@ module.exports = {
   siteUrl: `https://cookieblog.vercel.app`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: `https://github.com/korECM`, // Your GitHub account
+    github: `korECM`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
